6be8ad4cdef5b5eaf13f1d9e5e50599eeb16324d
Asel <peirisasel373@Gmail.com>
2022-01-08 11:39:39 +0530

    get suggested satellites endpoint.

--- src/api/satellite/controllers/satellite.js
+++ src/api/satellite/controllers/satellite.js
@@ -5,5 +5,23 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const axios = require('axios');
 
-module.exports = createCoreController('api::satellite.satellite');
+module.exports = createCoreController('api::satellite.satellite', ({ strapi }) => ({
+    // Obtaining nearby satellites for the user from n2yo api
+
+    async getSuggested(ctx) {
+        try {
+            const { observer_lat, observer_long, observer_alt, search_radius, category_id } = ctx.query
+            console.log(`lat: ${observer_lat} long:${observer_long} alt: ${observer_alt}, sr: ${search_radius}, ci: ${category_id}`)
+
+            const res = await axios.get(`${process.env.N2YO_BASE}/above/${observer_lat}/${observer_long}/${observer_alt}/${search_radius}/${category_id}
+                /?apiKey=${process.env.N2YO_KEY}`)
+
+            return res.data
+        } catch (err) {
+            console.log(err)
+            ctx.body = err;
+        }
+    },
+}));
